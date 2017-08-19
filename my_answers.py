d47078cead19c2b334d34c4cac1c98599e42e1dd
yawningtap <yawningtap@gmail.com>
2017-08-19 15:11:50 +0200

    Add files via upload

---
+++ my_answers.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import keras
+
+
+# TODO: fill out the function below that transforms the input series 
+# and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_series(series, window_size):
+    # containers for input/output pairs
+    X = []
+    y = []
+
+    x_len = len(series)
+    for i in range(x_len-window_size):
+        X.append(series[i:i+window_size])
+
+    y = series[window_size:]
+
+    # reshape each 
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:2])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+
+    return X,y
+
+# TODO: build an RNN to perform regression on our time series input/output data
+def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1) , return_sequences=False))
+    model.add(Dense(1))
+    return model
+
+
+### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+def cleaned_text(text):
+    punctuation = ['!', ',', '.', ':', ';', '?']
+    for c in text:
+        if not(c in punctuation) and not(c>='a' and c<='z'):
+            text = text.replace(c,' ')
+
+    return text
+
+### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_text(text, window_size, step_size):
+    # containers for input/output pairs
+    inputs = []
+    outputs = []
+
+    text_len = len(text)
+    x = 0
+    while x+window_size<text_len:
+        inputs.append(text[x:x+window_size])
+        outputs.append(text[x+window_size])
+        x += step_size
+
+    return inputs,outputs
+
+# TODO build the required RNN model: 
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+def build_part2_RNN(window_size, num_chars):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars) , return_sequences=False))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
